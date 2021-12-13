50854108a73023d87ac9a4180e153519ffa079ee
Rabin Adhikari <rabin.adk1@gmail.com>
2021-12-13 20:45:47 +0545

    Statically serve CSVs

--- server/app/__init__.py
+++ server/app/__init__.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from fastapi.staticfiles import StaticFiles
 
 # Mount routers before database to read their database models
 from .auth import router as auth_router
@@ -7,6 +8,8 @@ from .heroes import router as heroes_router
 from .database import create_tables  # isort: skip
 
 app = FastAPI()
+
+app.mount("/static", StaticFiles(directory="static"), name="static")
 
 
 @app.on_event("startup")
