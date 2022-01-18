945544705a89c368df28b55de44e2b93c315ecf6
Rabin Adhikari <rabin.adk1@gmail.com>
2022-01-18 18:34:59 +0545

    Add index to the username

--- server/app/auth/models.py
+++ server/app/auth/models.py
@@ -20,7 +20,9 @@ class UserBase(SQLModel):
     Base user model
     """
 
-    username: constr(strip_whitespace=True, to_lower=True, min_length=4, max_length=20)
+    username: constr(
+        strip_whitespace=True, to_lower=True, min_length=4, max_length=20
+    ) = Field(index=True)
     email: Optional[EmailStr] = None
     full_name: str
 
