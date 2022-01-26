13e10a59e027f8f2867d8f5896e38fc5b6c0eec0
Rabin Adhikari <rabin.adk1@gmail.com>
2022-01-26 18:39:29 +0545

    Upload latest build

--- server/app/__init__.py
+++ server/app/__init__.py
@@ -1,4 +1,6 @@
+import os.path
 from fastapi import FastAPI
+from fastapi.responses import FileResponse
 from fastapi.staticfiles import StaticFiles
 
 # Mount routers before database to read their database models
@@ -10,6 +12,7 @@ from .database import create_tables  # isort: skip
 
 app = FastAPI()
 
+
 app.mount("/static", StaticFiles(directory="static"), name="static")
 
 
@@ -18,13 +21,19 @@ def on_startup():
     create_tables()
 
 
-@app.get("/")
-async def root():
-    return {"message": "Hello World"}
-
-
 # Register API Routers here
 
 app.include_router(auth_router)
 app.include_router(heroes_router)
 app.include_router(tweets_router)
+
+# Directory to serve root files
+files_dir = "templates"
+
+
+@app.get("/{file_path:path}", response_class=FileResponse)
+async def index(file_path: str):
+    actual_path = os.path.join(files_dir, file_path)
+    if os.path.isfile(actual_path):
+        return actual_path
+    return os.path.join(files_dir, "index.html")
