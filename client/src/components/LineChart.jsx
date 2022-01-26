a74c3699c45900203107c9f9a895d91d9c25d819
Samip Poudel <074bct533.samip@pcampus.edu.np>
2022-01-26 18:59:49 +0545

    Added Charts

--- client/src/components/LineChart.jsx
+++ client/src/components/LineChart.jsx
@@ -6,15 +6,18 @@ import {
   PointElement,
   LineElement,
   Title,
+  ArcElement,
   Tooltip,
   Legend,
 } from 'chart.js';
-import { Line } from 'react-chartjs-2';
+import { Doughnut, Line, Pie } from 'react-chartjs-2';
 import axios from 'axios';
 import { baseAddress } from '../constants';
+import { Card } from '@mui/material';
 // import faker from 'faker';
 
 ChartJS.register(
+  ArcElement,
   CategoryScale,
   LinearScale,
   PointElement,
@@ -32,32 +35,28 @@ export const options = {
     },
     title: {
       display: true,
-      text: 'Chart.js Line Chart',
+      text: 'Line Chart',
     },
   },
 };
 
-const labels = ['January', 'February', 'March', 'April', 'May', 'June', 'July'];
-
-export const data = {
-  labels,
-  datasets: [
-    // {
-    //   label: 'Dataset 1',
-    //   data: labels.map((lab,index) => index),
-    //   borderColor: 'rgb(255, 99, 132)',
-    //   backgroundColor: 'rgba(255, 99, 132, 0.5)',
-    // },
-    {
-      label: 'Dataset 2',
-      data: labels.map((lab,index) => index),
-      borderColor: 'rgb(53, 162, 235)',
-      backgroundColor: 'rgba(53, 162, 235, 0.5)',
+export const optionsPie = {
+  responsive: true,
+  plugins: {
+    legend: {
+      // position: 'top',
+      display:true
     },
-  ],
+    title: {
+      display: true,
+      text: 'Pie Chart',
+    },
+  },
 };
 
-const fetchLabels = (month)=>{
+
+
+const fetchLabels = async (month)=>{
 let covid_stats = []
 let vaccination = []
 let covid_politics= []
@@ -82,46 +81,46 @@ return axios.get(`/tweets/overview`).then((data)=>data.data).then((data)=>{
   finalData["datasets"]=[{
     label: 'Covid Stats',
     data: covid_stats,
-    borderColor: 'rgb(53, 162, 235)',
-    backgroundColor: 'rgba(53, 162, 235, 0.5)',
+    borderColor: 'rgb(19, 138, 51)',
+    backgroundColor: 'rgba(19, 138, 51, 0.5)',
   },
   {
     label: 'Vaccination',
     data: vaccination,
-    borderColor: 'rgb(53, 162, 35)',
-    backgroundColor: 'rgba(53, 162, 35, 0.5)',
+    borderColor: 'rgb(255, 3, 226)',
+    backgroundColor: 'rgba(255, 3, 226, 0.5)',
   },
   {
     label: 'Covid Politics',
     data: covid_politics,
-    borderColor: 'rgb(53, 225, 235)',
-    backgroundColor: 'rgba(53, 225, 235, 0.5)',
+    borderColor: 'rgb(235, 128, 52)',
+    backgroundColor: 'rgba(235, 128, 52, 0.5)',
   },
   {
     label: 'Humour',
     data: humour,
-    borderColor: 'rgb(153, 162, 235)',
-    backgroundColor: 'rgba(153, 162, 235, 0.5)',
+    borderColor: 'rgb(195, 235, 52)',
+    backgroundColor: 'rgba(195, 235, 52, 0.5)',
   },{
     label: 'Lockdown',
     data: lockdown,
-    borderColor: 'rgb(253, 162, 235)',
-    backgroundColor: 'rgba(253, 162, 235, 0.5)',
+    borderColor: 'rgb(0, 174, 255)',
+    backgroundColor: 'rgba(0, 174, 255, 0.5)',
   },{
     label: 'Civic Views',
     data: civic_views,
-    borderColor: 'rgb(3, 33, 235)',
-    backgroundColor: 'rgba(3, 33, 235, 0.5)',
+    borderColor: 'rgb(255, 36, 3)',
+    backgroundColor: 'rgba(255, 36, 3, 0.5)',
   },{
     label: 'Life during Pandemic',
     data: life_during_pandemic,
-    borderColor: 'rgb(53, 12, 235)',
-    backgroundColor: 'rgba(53, 12, 235, 0.5)',
+    borderColor: 'rgb(255, 3, 129)',
+    backgroundColor: 'rgba(255, 3, 129, 0.5)',
   },{
     label: 'Covid Waves and Variants',
     data: covid_waves_and_variants,
-    borderColor: 'rgb(53, 162, 23)',
-    backgroundColor: 'rgba(53, 162, 23, 0.5)',
+    borderColor: 'rgb(255, 196, 3)',
+    backgroundColor: 'rgba(255, 196, 3, 0.5)',
   },
   {
     label: 'Misinformation',
@@ -138,13 +137,74 @@ console.log(finalData)
 }
 export function LineChart() {
 const [labels, setLabels] = useState({})
-  useEffect(async ()=>{
+const [loading, setLoading] = useState(false)
+const pieLabels = ["Covid Stats","Vaccination","Covid Politics","Humour","Lockdown","Civic Views","Life During Pandemic","Covid Waves and Variants","Misinformation"]
+const [pieData, setPieData] = useState({labels:pieLabels})
+useEffect(async ()=>{
   const label2 = await fetchLabels()
+    if(label2.labels){
+
+      let dataTemp = label2.datasets.map((datum=>datum.data.reduce((prev,curr)=>prev+curr,0)))
+      setPieData({
+        labels:pieLabels,
+        datasets:[{
+          label:'Pie Chart',
+          data : dataTemp,
+          backgroundColor: [
+            'rgb(19, 138, 51)',
+            'rgb(255, 3, 226)',
+            'rgb(235, 128, 52)',
+            'rgb(195, 235, 52)',
+            'rgb(0, 174, 255)',
+            'rgb(255, 36, 3)',
+            'rgb(255, 3, 129)',
+            'rgb(255, 196, 3)',
+            'rgb(53, 16, 23)',
+          ],
+        }]
+      })  
+    }
     setLabels(label2)
+    setLoading(true)
   },[])
+// useEffect(()=>{
+
+// if(labels.labels){
+
+//   let dataTemp = labels.datasets.map((datum=>datum.data.reduce((prev,curr)=>prev+curr,0)))
+//   console.log(dataTemp)
+//   setPieData({
+//     labels:pieLabels,
+//     datasets:{
+//       label:'Pie Chart',
+//       data : dataTemp,
+//       backgroundColor: [
+//         'rgb(255, 99, 132)',
+//         'rgb(54, 62, 235)',
+//         'rgb(2, 12, 235)',
+//         'rgb(5, 162, 235)',
+//         'rgb(54, 162, 25)',
+//         'rgb(4, 162, 25)',
+//         'rgb(54, 12, 235)',
+//         'rgb(54, 2, 25)',
+//         'rgb(255, 205, 86)'
+//       ],
+//     }
+//   })  
+// }
+// },[labels.datasets])
+console.log(pieData)
+
   return (
-  <div>
-  {labels.labels && <Line options={options} data={labels} />}
+  <div className='flex w-11/12 my-3 mx-16'>
+  {loading && 
+  <Card className='flex-1'>
+  <Line options={options} data={labels} />
+  </Card>}
+  {loading && 
+  <Card className='w-1/3 ml-3'>
+  <Pie options={optionsPie} data={pieData}/>
+  </Card>}
   </div>
   );
 }
