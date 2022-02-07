b78c3212f2fdb042cf7f6dda54686c3b58a7bdf1
Samip Poudel <074bct533.samip@pcampus.edu.np>
2022-02-07 21:26:09 +0545

    Slight change is color pallete

--- client/src/components/LineChart.jsx
+++ client/src/components/LineChart.jsx
@@ -1,196 +1,168 @@
-import { useEffect, useState } from "react";
+import {useEffect, useState} from "react";
 import {
-  Chart as ChartJS,
-  CategoryScale,
-  LinearScale,
-  PointElement,
-  LineElement,
-  Title,
-  ArcElement,
-  Tooltip,
-  Legend,
+    Chart as ChartJS,
+    CategoryScale,
+    LinearScale,
+    PointElement,
+    LineElement,
+    Title,
+    ArcElement,
+    Tooltip,
+    Legend,
 } from "chart.js";
-import { Line, Pie } from "react-chartjs-2";
+import {Line, Pie} from "react-chartjs-2";
 import axios from "axios";
-import { Card } from "@mui/material";
+import {Card} from "@mui/material";
+import {columns} from "../constants";
+
 // import faker from 'faker';
 
 ChartJS.register(
-  ArcElement,
-  CategoryScale,
-  LinearScale,
-  PointElement,
-  LineElement,
-  Title,
-  Tooltip,
-  Legend
+    ArcElement,
+    CategoryScale,
+    LinearScale,
+    PointElement,
+    LineElement,
+    Title,
+    Tooltip,
+    Legend,
 );
 
 const options = {
-  responsive: true,
-  plugins: {
-    legend: {
-      position: "top",
+    responsive: true,
+    plugins: {
+        legend: {
+            position: "top",
+        },
+        title: {
+            display: true,
+            text: "Line Chart",
+        },
     },
-    title: {
-      display: true,
-      text: "Line Chart",
-    },
-  },
 };
 
 const optionsPie = {
-  responsive: true,
-  plugins: {
-    legend: {
-      // position: 'top',
-      display: true,
+    responsive: true,
+    plugins: {
+        legend: {
+            // position: 'top',
+            display: true,
+        },
+        title: {
+            display: true,
+            text: "Pie Chart",
+        },
     },
-    title: {
-      display: true,
-      text: "Pie Chart",
-    },
-  },
 };
 
 const fetchLabels = async () =>
-  axios
-    .get(`/tweets/overview?all=True`)
-    .then((data) => data.data)
-    .then((data) => {
-      const finalData = {};
-      const covid_stats = data.map((datum) => datum.covid_stats);
-      const vaccination = data.map((datum) => datum.vaccination);
-      const covid_politics = data.map((datum) => datum.covid_politics);
-      const humour = data.map((datum) => datum.humour);
-      const lockdown = data.map((datum) => datum.lockdown);
-      const civic_views = data.map((datum) => datum.civic_views);
-      const life_during_pandemic = data.map(
-        (datum) => datum.life_during_pandemic
-      );
-      const covid_waves_and_variants = data.map(
-        (datum) => datum.covid_waves_and_variants
-      );
-      finalData["labels"] = data.map((datum) => datum.created_date);
-      finalData["datasets"] = [
-        {
-          label: "Covid Stats",
-          data: covid_stats,
-          borderColor: "rgb(19, 138, 51)",
-          backgroundColor: "rgba(19, 138, 51, 0.5)",
-        },
-        {
-          label: "Vaccination",
-          data: vaccination,
-          borderColor: "rgb(255, 3, 226)",
-          backgroundColor: "rgba(255, 3, 226, 0.5)",
-        },
-        {
-          label: "Covid Politics",
-          data: covid_politics,
-          borderColor: "rgb(235, 128, 52)",
-          backgroundColor: "rgba(235, 128, 52, 0.5)",
-        },
-        {
-          label: "Humour",
-          data: humour,
-          borderColor: "rgb(195, 235, 52)",
-          backgroundColor: "rgba(195, 235, 52, 0.5)",
-        },
-        {
-          label: "Lockdown",
-          data: lockdown,
-          borderColor: "rgb(0, 174, 255)",
-          backgroundColor: "rgba(0, 174, 255, 0.5)",
-        },
-        {
-          label: "Civic Views",
-          data: civic_views,
-          borderColor: "rgb(255, 36, 3)",
-          backgroundColor: "rgba(255, 36, 3, 0.5)",
-        },
-        {
-          label: "Life during Pandemic",
-          data: life_during_pandemic,
-          borderColor: "rgb(255, 3, 129)",
-          backgroundColor: "rgba(255, 3, 129, 0.5)",
-        },
-        {
-          label: "Covid Waves and Variants",
-          data: covid_waves_and_variants,
-          borderColor: "rgb(255, 196, 3)",
-          backgroundColor: "rgba(255, 196, 3, 0.5)",
-        },
-      ];
-      console.log(finalData);
-      return finalData;
-    })
-    .catch((error) => {
-      console.log(error);
-    });
+    axios
+        .get(`/pseudo_tweets/overview?all=true`)
+        .then((data) => data.data)
+        .then((data) => {
+            const finalData = {};
+            const covid_stats = data.map((datum) => datum.covid_stats);
+            const vaccination = data.map((datum) => datum.vaccination);
+            const covid_politics = data.map((datum) => datum.covid_politics);
+            const humour = data.map((datum) => datum.humour);
+            const lockdown = data.map((datum) => datum.lockdown);
+            const civic_views = data.map((datum) => datum.civic_views);
+            const life_during_pandemic = data.map(
+                (datum) => datum.life_during_pandemic,
+            );
+            const covid_waves_and_variants = data.map(
+                (datum) => datum.covid_waves_and_variants,
+            );
+            const dataArrays = {
+                covid_stats: covid_stats,
+                vaccination: vaccination,
+                covid_politics: covid_politics,
+                humour: humour,
+                lockdown: lockdown,
+                civic_views: civic_views,
+                life_during_pandemic: life_during_pandemic,
+                covid_waves_and_variants: covid_waves_and_variants,
+            };
+            finalData["labels"] = data.map((datum) => datum.created_date);
+            finalData["datasets"] = columns
+                .filter(
+                    (column) =>
+                        column.field !== "text" && column.field !== "verify",
+                )
+                .map((column) => {
+                    return {
+                        data: dataArrays[column.field],
+                        label: column.label,
+                        borderColor: column.borderColor,
+                        backgroundColor: column.backgroundColor,
+                    };
+                });
+
+            console.log(finalData);
+            return finalData;
+        })
+        .catch((error) => {
+            console.log(error);
+        });
 
 function LineChart() {
-  const [labels, setLabels] = useState({});
-  const [loading, setLoading] = useState(false);
+    const [labels, setLabels] = useState({});
+    const [loading, setLoading] = useState(false);
 
-  const [pieData, setPieData] = useState({ labels: [] });
-  useEffect(() => {
-    const pieLabels = [
-      "Covid Stats",
-      "Vaccination",
-      "Covid Politics",
-      "Humour",
-      "Lockdown",
-      "Civic Views",
-      "Life During Pandemic",
-      "Covid Waves and Variants",
-    ];
-    fetchLabels().then((label2) => {
-      if (label2.labels) {
-        const dataTemp = label2.datasets.map((datum) =>
-          datum.data.reduce((prev, curr) => prev + curr, 0)
-        );
-        setPieData({
-          labels: pieLabels,
-          datasets: [
-            {
-              label: "Pie Chart",
-              data: dataTemp,
-              backgroundColor: [
-                "rgb(19, 138, 51)",
-                "rgb(255, 3, 226)",
-                "rgb(235, 128, 52)",
-                "rgb(195, 235, 52)",
-                "rgb(0, 174, 255)",
-                "rgb(255, 36, 3)",
-                "rgb(255, 3, 129)",
-                "rgb(255, 196, 3)",
-                "rgb(53, 16, 23)",
-              ],
-            },
-          ],
+    const [pieData, setPieData] = useState({labels: []});
+    useEffect(() => {
+        const pieLabels = [
+            "Covid Stats",
+            "Vaccination",
+            "Covid Politics",
+            "Humour",
+            "Lockdown",
+            "Civic Views",
+            "Life During Pandemic",
+            "Covid Waves and Variants",
+        ];
+        fetchLabels().then((label2) => {
+            if (label2.labels) {
+                const dataTemp = label2.datasets.map((datum) =>
+                    datum.data.reduce((prev, curr) => prev + curr, 0),
+                );
+                setPieData({
+                    labels: pieLabels,
+                    datasets: [
+                        {
+                            label: "Pie Chart",
+                            data: dataTemp,
+                            backgroundColor: columns
+                                .filter(
+                                    (column) =>
+                                        column.field !== "text" &&
+                                        column.field !== "verify",
+                                )
+                                .map((column) => column.backgroundColor),
+                        },
+                    ],
+                });
+            }
+            setLabels(label2);
+            setLoading(true);
         });
-      }
-      setLabels(label2);
-      setLoading(true);
-    });
-  }, []);
+    }, []);
 
-  return (
-    <div className="flex w-11/12 my-3 mx-16">
-      {loading && (
-        <Card className="flex-1">
-          <Line options={options} data={labels} />
-        </Card>
-      )}
-      {loading && (
-        <Card className="w-1/3 ml-3">
-          <Pie options={optionsPie} data={pieData} />
-        </Card>
-      )}
-    </div>
-  );
+    return (
+        <div className="flex w-11/12 my-3 mx-16">
+            {loading && (
+                <Card className="flex-1">
+                    <Line options={options} data={labels} />
+                </Card>
+            )}
+            {loading && (
+                <Card className="w-1/3 ml-3">
+                    <Pie options={optionsPie} data={pieData} />
+                </Card>
+            )}
+        </div>
+    );
 }
 
-export { options, optionsPie };
+export {options, optionsPie};
 export default LineChart;
--- client/src/constants.js
+++ client/src/constants.js
@@ -1,41 +1,66 @@
 const baseAddress = "http://localhost:8000";
 const columns = [
-  { field: "verify", headerName: "Verify" },
-  {
-    field: "text",
-    headerName: "text",
-  },
-  {
-    field: "covid_stats",
-    headerName: "covid \n stats",
-  },
-  {
-    field: "vaccination",
-    headerName: "vaccination",
-  },
-  {
-    field: "covid_politics",
-    headerName: "covid \n politics",
-  },
-  {
-    field: "humour",
-    headerName: "humour",
-  },
-  {
-    field: "lockdown",
-    headerName: "lockdown",
-  },
-  {
-    field: "civic_views",
-    headerName: "civic \n views",
-  },
-  {
-    field: "life_during_pandemic",
-    headerName: "life during \npandemic",
-  },
-  {
-    field: "covid_waves_and_variants",
-    headerName: "covid waves \n and \n variants",
-  },
+    {field: "verify", label: "Verify", headerName: "Verify"},
+    {
+        field: "text",
+        label: "Text",
+        Headername: "text",
+    },
+    {
+        field: "covid_stats",
+        label: "Covid Stats",
+        borderColor: "#390ba4",
+        backgroundColor: "#390ba4",
+        headerName: "covid \n stats",
+    },
+    {
+        field: "vaccination",
+        label: "Vaccination",
+        borderColor: "#8c0096",
+        backgroundColor: "#8c0096",
+        headerName: "vaccination",
+    },
+    {
+        field: "covid_politics",
+        label: "Covid Politics",
+        borderColor: "#bf0082",
+        backgroundColor: "#bf0082",
+        headerName: "covid \n politics",
+    },
+    {
+        field: "humour",
+        label: "Humour",
+        borderColor: "#e2006a",
+        backgroundColor: "#e2006a",
+        headerName: "humour",
+    },
+    {
+        field: "lockdown",
+        label: "Lockdown",
+        borderColor: "#f90151",
+        backgroundColor: "#f90151",
+        headerName: "lockdown",
+    },
+    {
+        field: "civic_views",
+        label: "Civic Views",
+        borderColor: "#ff5039",
+        backgroundColor: "#ff5039",
+        headerName: "civic \n views",
+    },
+    {
+        field: "life_during_pandemic",
+        label: "Life During Pandemic",
+        borderColor: "#ff7e1f",
+        backgroundColor: "#ff7e1f",
+        headerName: "Life During \nPandemic",
+    },
+    {
+        field: "covid_waves_and_variants",
+        borderColor: "#ffa600",
+        backgroundColor: "#ffa600",
+        label: "Covid Waves and Variants",
+        headerName: "Covid Waves \n and \n Variants",
+    },
 ];
-export { baseAddress,columns };
+export {baseAddress, columns};
