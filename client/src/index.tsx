f3a22a7058dc90b8f48c5aeb9ca31044b7a711db
Rabin Adhikari <rabin.adk1@gmail.com>
2022-04-16 10:06:38 +0545

    Frontend: Change renderer for react18

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -1,18 +1,19 @@
 import { StrictMode } from "react";
-import { render } from "react-dom";
+import { createRoot } from "react-dom/client";
 // import './styles/tailwind.css';
 import "./index.css";
 import App from './App';
 import { BrowserRouter } from 'react-router-dom';
 
-render(
+const container = document.getElementById('root');
+const root = createRoot(container!);
+
+root.render(
   <StrictMode>
     <BrowserRouter>
     <App />
     </BrowserRouter>
-  </StrictMode>,
-  document.getElementById('root')
-
+  </StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
