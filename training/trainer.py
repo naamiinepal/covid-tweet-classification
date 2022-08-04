2a8d5f207ec8867d9a336054222a7b4cd11542e6
Rabin Adhikari <rabin.adk1@gmail.com>
2022-08-04 19:21:30 +0545

    Add configs and omegaconf

--- training/trainer.py
+++ training/trainer.py
@@ -5,4 +5,8 @@ from pytorch_lightning.utilities.cli import LightningCLI
 from model import LightningModel
 from datamodule import DataModule
 
-cli = LightningCLI(LightningModel, DataModule)
+cli = LightningCLI(
+    LightningModel,
+    DataModule,
+    parser_kwargs={"parser_mode": "omegaconf"},
+)
